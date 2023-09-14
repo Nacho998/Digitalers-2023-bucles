008fcff8522be499ab6fc2c060ee8545da9546db
Ignacio Valdez <valdezignacio998@gmail.com>
2023-09-14 20:00:13 -0300

    archivo do while

---
+++ js/do-while.js
@@ -0,0 +1,17 @@
+/*let estacion = prompt('Ingrese una estacion del año:').toLowerCase
+
+while (estacion != 'verano' && estacion != 'invierno' && estacion != 'otoño' && estacion != 'primavera') {
+
+    estacion = prompt('Ingrese una estacion del año:')
+
+}*/
+
+let edadCliente = parseInt(prompt('Ingrese la edad del cliente:'))
+
+while (isNaN(edadCliente) || edadCliente > 100 || edadCliente <= 0) {
+
+    edadCliente = parseInt(prompt('Reingrese la edad del cliente:'))
+
+}
+
+console.log(edadCliente)
