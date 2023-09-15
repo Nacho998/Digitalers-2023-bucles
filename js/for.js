0bfbc26911020d4887a537a5d87a105a413ddc1b
Ignacio Valdez <valdezignacio998@gmail.com>
2023-09-14 21:56:55 -0300

    for y for of

---
+++ js/for.js
@@ -0,0 +1,25 @@
+const alumnosCurso = ['Mara', 'Maria', 'Mel', 'Ana', 'Agustina', 'Flor', 'Anto', 'Pipi', 'Mostra']
+
+
+// ? FOR CONVENCIONAL
+for (let i = 0; i < alumnosCurso.length; i++) {
+
+    console.log(`alumno: ${alumnosCurso[i]}`)
+
+}
+
+
+// ? FOR OF
+for (let alumno of alumnosCurso) {
+    console.log(alumno)
+}
+
+
+
+
+
+/*for (let i = 1; i <= 100; i++) {
+
+    console.log(`Iteracion numero ${i}`)
+
+}*/
